1fecf537c41ce35fb16221277b970dfe019e0965
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-22 23:30:39 +0530

    Employee is absent or present

---
+++ Emp_UC1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReport
+{ 
+    public class EmployeeAbsentyCheck
+    {
+    public static void EmpAbsenty()
+    {
+        Random random = new Random();
+        int num = random.Next(2);
+
+        if (num == 1)
+        {
+            Console.WriteLine("Employee is present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent");
+        }
+      }
+   }
+}
+
+
