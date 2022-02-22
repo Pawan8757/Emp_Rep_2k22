46db961cce3a4bc52fda95225194af251f86f36f
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-22 23:28:28 +0530

    adding welcome file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeReport;
+using System;
+
+namespace EmpReport;
+class program
+{
+    public static void Main(string[] args)
+    {
+
+        //EmpPerMont.EmpMonHr();
+
+        //EmpSwitch.CheckUsingSwitchCase();
+        //EmployeeAbsentyCheck.EmpAbsenty();
+        //EmpWageDaily.CheckEmpWage();
+        EmpDailyPartTimeWage.CheckWages();
+
+
+
+
+    }
+}
