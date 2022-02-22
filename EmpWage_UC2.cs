273fa3deb8578cd95b77812475a685202186106e
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-22 23:32:44 +0530

    daily employee wages

---
+++ EmpWage_UC2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReport
+{
+    public class EmpWageDaily
+    {
+        public static void CheckEmpWage()
+        {
+            int WagePerHr = 20;
+            int FullWorkingDays = 8;
+
+            Random random = new Random();
+            int number = random.Next(2);
+
+            if (number == 1)
+            {
+                int calculated = WagePerHr * FullWorkingDays;
+                Console.WriteLine(calculated);
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+            }
+        }
+    }
+}
